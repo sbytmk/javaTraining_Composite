16f98b5cfd7d9527e868348192e510f7aad86a14
sbytmk <sbytmk@192.168.1.5>
2013-09-19 05:51:17 +0900

    java言語で学ぶデザインパターン入門　第11章　Composite　容器と中身の同一視

---
+++ src/Entry.java
@@ -0,0 +1,19 @@
+public abstract class Entry {
+	public abstract String getName(); // 名前を得る
+
+	public abstract int getSize(); // サイズを得る
+
+	public Entry add(Entry entry) throws FileTreatmentException { // エントリを追加する
+		throw new FileTreatmentException();
+	}
+
+	public void printList() { // 一覧を表示する
+		printList("");
+	}
+
+	protected abstract void printList(String prefix); // prefixを前につけて一覧を表示する
+
+	public String toString() {
+		return getName() + "(" + getSize() + ")";
+	}
+}
